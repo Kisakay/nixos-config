5f3c170c5717cfa4e8dcfc7568ed98d7cf76fceb
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-10-02 13:12:16 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -37,7 +37,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "anais-laptop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
