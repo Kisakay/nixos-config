c3a8393a62d383245688c5957b3470e09931b7b4
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2025-12-31 12:37:02 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -566,11 +566,18 @@
     # POSTGRESQL
     postgresql = {
       enable = true;
-      package = pkgs.postgresql_16; # Choisir la version
+      package = pkgs.postgresql_16;
+
       initialScript = pkgs.writeText "init.sql" ''
         CREATE DATABASE mydb;
         CREATE USER myuser WITH PASSWORD 'mypassword';
         GRANT ALL PRIVILEGES ON DATABASE mydb TO myuser;
+
+        \\c mydb
+
+        GRANT USAGE, CREATE ON SCHEMA public TO myuser;
+        ALTER DEFAULT PRIVILEGES IN SCHEMA public
+          GRANT ALL ON TABLES TO myuser;
       '';
     };
 
