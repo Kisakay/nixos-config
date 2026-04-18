9f4c036ff6cb215a206fdbe893ec783e54ebbfb6
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-04-18 14:05:17 +0200

    Update wireguard-secrets.example.nix

--- wireguard-secrets.example.nix
+++ wireguard-secrets.example.nix
@@ -1,16 +1,26 @@
 {
-  privateKey = "<private key>";
-  presharedKey = "<preshared key>";
-  publicKey = "<public key>";
-  serverPort = "<server port>";
-  serverIp = "<server ip>";
-  wg0AddressV4 = "<wg0 address (ipv4) with CIDR>";
-  wg0AddressV6 = "<wg0 address (ipv6) with CIDR>";
-  dns1 = "<first dns>";
-  dns2 = "<second dns>";
-  allowedIPs = [
-    "0.0.0.0/0"
-    "::/0"
-  ];
-  persistentKeepalive = 25;
+  my.wireguard.wg0 = {
+    enable = true;
+
+    serverIp = "";
+    serverPort = "";
+
+    wg0AddressV4 = "";
+    wg0AddressV6 = "";
+
+    dns1 = "1.1.1.1";
+    dns2 = "1.0.0.1";
+
+    publicKey = "";
+
+    allowedIPs = [
+      "0.0.0.0/0"
+      "::/0"
+    ];
+
+    persistentKeepalive = 25;
+
+    privateKeyFile = /etc/nixos-local/wireguard/privateKeyFile_wg0.key;
+    presharedKeyFile = /etc/nixos-local/wireguard/presharedKeyFile_wg0.key;
+  };
 }
