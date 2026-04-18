165d2d2c90e8515bd42a8942714b38cffef90200
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-04-18 14:00:44 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,15 @@
 {
   inputs = {
     pelican.url = "github:Hythera/nix-pelican";
-    nixos-local.url = "path:/etc/nixos-local";
+
+    nixos-local = {
+      url = "path:/etc/nixos-local";
+      flake = false;
+    };
   };
 
-  outputs = { nixpkgs, home-manager, pelican, nixos-local, ... }: let
+  outputs = { nixpkgs, pelican, nixos-local, ... }: let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
   in {
     nixosConfigurations."computer" = nixpkgs.lib.nixosSystem {
       inherit system;
@@ -14,7 +17,7 @@
         pelican.nixosModules.default
         { nixpkgs.overlays = [ pelican.overlays.default ]; }
         ./configuration.nix
-        (nixos-local + /wireguard/wg0-secrets.nix)
+        "${nixos-local}/wireguard/wg0-secrets.nix"
       ];
     };
   };
