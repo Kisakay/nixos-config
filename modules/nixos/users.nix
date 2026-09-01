d60f9cbe4346c07879bac2c00a9c0581b0e183ff
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2026-09-01 10:08:12 +0200

    allowing framework 12 connecting to ssh

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -5,6 +5,9 @@ in
   users.users.${System.Username} = {
     isNormalUser = true;
     description = System.Fullname;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAN4jjXCrvgOwttSLKl6uRFbWMolKWNSfZ4Dxa/QZDuO kisa@framework"
+    ];
     extraGroups = [
       "networkmanager"
       "wheel"
