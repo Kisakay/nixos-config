af3b15f516c9fc6960328ce11da0eafea1a7a077
Anais Saraiva <anais.saraiva@ihorizon.org>
2026-09-02 09:43:55 +0200

    Update flatpak.nix

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -2,4 +2,12 @@
 
 {
 services.flatpak.enable = true;
+
+services.flatpak.remotes = [{
+  name = "flathub-beta"; location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+}];
+
+services.flatpak.packages = [
+  "com.github.flxzt.rnote"
+];
 }
